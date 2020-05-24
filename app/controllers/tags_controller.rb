846e3a91ee09c740a924a4856920035766e4ea59
Robb <robb.tate@gmail.com>
2020-05-24 20:40:29 +0100

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,14 @@
+class TagsController < ApplicationController
+    def show
+        @tag = Tag.find(params[:id])
+    end
+    def index
+        @tags = Tag.all
+    end
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        flash.notice = "Tag '#{@tag.name}' Destroyed!"
+        redirect_to articles_path(@article)
+      end
+end
